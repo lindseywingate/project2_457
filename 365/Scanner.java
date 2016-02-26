8bdc36ac76293b31d6d2147c58761ddcd89d0940
Lindsey Wingate <lindseymwingate@gmail.com>
2016-02-26 09:35:42 -0600

    lots of success! just not finding the id between symbols

--- 365/Scanner.java
+++ 365/Scanner.java
@@ -14,47 +14,48 @@ class Scanner
 		BufferedReader br = new BufferedReader (new InputStreamReader(streamer));
 
 		String line = null;
-
-		String read = "^read";	
-		String write = "^write";
-		String id = "[a-zA-Z]*[a-zA-Z0-9]";
+		String read = "read";	
+		String write = "write";
+		String id = "[a-zA-Z][a-zA-z0-9]*";
 		String lparen = "[(]";
 		String rparen = "[)]";
+		String assign = "[:][=]";
 		String add_op = "[+-]";	
 		String mult_op = "[/*|//|///|/%]";
-		String assign = "[:][=]";
 		String num = "[0-9]*[\\.]*[0-9]";
 
 		Pattern r = Pattern.compile(read);
-		Pattern s = Pattern.compile(write); 
+		Pattern s = Pattern.compile(write);
+		Pattern c = Pattern.compile(id); 
 		Pattern i = Pattern.compile(lparen);
 		Pattern j = Pattern.compile(rparen);
+		Pattern a = Pattern.compile(assign);	
 		Pattern k = Pattern.compile(add_op);
 		Pattern l = Pattern.compile(mult_op);
-		Pattern a = Pattern.compile(assign);	
 		Pattern b = Pattern.compile(num);
 
 		while ((line = br.readLine()) !=null) {
 			//System.out.println(line);
 			Matcher m = r.matcher(line);
 			if(m.find( )) {
-				System.out.println("<read>, read");			
+				System.out.println("<read>, read");		
+				line = line.replace("read", "");	
 			}
 			Matcher n = s.matcher(line);
 			if(n.find()) {
 				System.out.println("<write>, write");
 			}	
-		//	Matcher o = i.matcher(line);
-		//	if(o.find()) {
-		//		System.out.println("<id>" + o.group(0));
-		//	}
 			Matcher p = i.matcher(line);
 			if(p.find()) {
 				System.out.println("<lparen>, " + p.group(0));
 			}	
-			Matcher q = j.matcher(line);
-			if(q.find()) {
-				System.out.println("<rparen>, " + q.group(0));
+			Matcher o = c.matcher(line);
+			if(o.find()) {
+				System.out.println("<id>, " + o.group(0));
+			}
+			Matcher x = a.matcher(line);
+			if(x.find()) {
+				System.out.println("<assign>, " + x.group(0));
 			}
 			Matcher z = k.matcher(line);
 			if(z.find()) {
@@ -64,13 +65,13 @@ class Scanner
 			if(y.find()) {
 				System.out.println("<mult_op>, " + y.group(0));
 			} 
-			Matcher x = a.matcher(line);
-			if(x.find()) {
-				System.out.println("<assign>, " + x.group(0));
-			}
 			Matcher w = b.matcher(line);
 			if(w.find()) {
 				System.out.println("<number>, " + w.group(0));
+			}
+			Matcher q = j.matcher(line);
+			if(q.find()) {
+				System.out.println("<rparen>, " + q.group(0));
 			}
 		}	
 		br.close();
