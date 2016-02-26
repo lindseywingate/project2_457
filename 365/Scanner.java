144d9a7f7434725775c88c7a6adc09dc05389491
Lindsey Wingate <lindseymwingate@gmail.com>
2016-02-26 13:19:41 -0600

    trimmed and added ^ to all searches

--- 365/Scanner.java
+++ 365/Scanner.java
@@ -16,14 +16,14 @@ class Scanner
 		String line = null;
 		String read = "read";	
 		String write = "write";
-		String id = "[a-zA-Z][a-zA-z0-9]*";
+		String id = "^[a-zA-Z][a-zA-z0-9]*";
 		String lparen = "[(]";
 		String rparen = "[)]";
 		String assign = "[:][=]";
-		String add_op = "[+-]";	
-		String mult_op = "[/*|//|///|/%]";
-		String number = "[0-9]*[\\.]*[0-9]";
-		String operators = "[+-/*|//|///|/%]";
+		String add_op = "^[+-]";	
+		String mult_op = " ^[/*|//|///|/%]";
+		String number = "^[0-9]*[\\.]*[0-9]";
+		String operators = "^[+-/*|//|///|/%]";
 
 		Pattern r = Pattern.compile(read);
 		Pattern s = Pattern.compile(write);
@@ -40,30 +40,33 @@ class Scanner
 			//System.out.println(line);
 			Matcher m = r.matcher(line);
 			if(m.find( )) {
-				//System.out.println("<read>, read");		
+				System.out.println("<read>, read");		
 				line = line.replace("read", "");	
+				line = line.trim();
 			}
 			Matcher n = s.matcher(line);
 			if(n.find()) {
 				System.out.println("<write>, write");
 				line = line.replace("write", "");
+				line = line.trim();
 			}	
 			Matcher p = i.matcher(line);
 			if(p.find()) {
 				System.out.println("<lparen>, " + p.group(0));
 				line = line.replace("(", "");
+				line = line.trim();
 			}	
 			Matcher o = c.matcher(line);
 			if(o.find()) {
 				System.out.println("<id>, " + o.group(0));
 				line = line.replace(o.group(0), "");
-				System.out.println("LINE: "+line);
+				line = line.trim();
 			}
 			Matcher x = a.matcher(line);
 			if(x.find()) {
 				System.out.println("<assign>, " + x.group(0));
 				line = line.replace(x.group(0), "");
-				System.out.println("LINE_assign" + line);
+				line = line.trim();
 			}		
 				Matcher oper = op.matcher(line);
 				while(oper.find()) {
@@ -72,62 +75,63 @@ class Scanner
 					if(d.find()) {
 						System.out.println("<id>, " + d.group(0));
 						line = line.replace(d.group(0), "");
-						System.out.println("LINE: "+line);
-						break;
-					}
-					Matcher num = b.matcher(line);
-					if(num.find()) {
-						System.out.println("<number>, " + num.group(0));
-						line = line.replace(num.group(0), "");
-						break;
+						line = line.trim();
 					}
 					Matcher add = k.matcher(line);
 					if(add.find()) {
 						System.out.println("<add_op>, " + add.group(0));
 						line = line.replace(add.group(0), "");
-						break;
+						line = line.trim();
+					}
+					Matcher num = b.matcher(line);
+					if(num.find()) {
+						System.out.println("<number>, " + num.group(0));
+						line = line.replace(num.group(0), "");
+						line = line.trim();
 					}
 					Matcher mult = l.matcher(line);
 					if(mult.find()){
 						System.out.println("<mult_op>, " + mult.group(0));
 						line = line.replace(mult.group(0), "");
-						break;
+						line = line.trim();
 					} 
 			}
-	/*	
 			Matcher id2 = c.matcher(line);
 			if(id2.find()) {
 				System.out.println("<id>, " + id2.group(0));
 				line = line.replace(id2.group(0), "");
-				System.out.println("LINE: "+line);
-				break;
+				line = line.trim();
 			}
 			Matcher num2 = b.matcher(line);
 			if(num2.find()) {
 				System.out.println("<number>, " + num2.group(0));
 				line = line.replace(num2.group(0), "");
-				break;
+				line = line.trim();
 			}
-	*/		
+			
 			Matcher z = k.matcher(line);
 			if(z.find()) {
 				System.out.println("<add_op>, " + z.group(0));
 				line = line.replace(z.group(0), "");
+				line = line.trim();
 			}
 			Matcher y = l.matcher(line);
 			if(y.find()) {
 				System.out.println("<mult_op>, " + y.group(0));
 				line = line.replace(y.group(0), "");
-			} 
+				line = line.trim();
+			}
 			Matcher w = b.matcher(line);
 			if(w.find()) {
 				System.out.println("<number>, " + w.group(0));
 				line = line.replace(w.group(0), "");
+				line = line.trim();
 			}
 			Matcher q = j.matcher(line);
 			if(q.find()) {
 				System.out.println("<rparen>, " + q.group(0));
 				line = line.replace(q.group(0), "");
+				line = line.trim();
 			}
 		}	
 		br.close();
