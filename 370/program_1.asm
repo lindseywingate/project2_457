29c2cf160960dd2057eb70db3ca73be2ef211ed0
Lindsey Wingate <lindseymwingate@gmail.com>
2016-06-14 09:40:39 -0500

    finally got ships printing out/asking correctly with help from wenchen!!

--- 370/program_1.asm
+++ 370/program_1.asm
@@ -20,142 +20,133 @@
              .half   218, 220, 222, 224, 226, 228
              .half   267, 269, 271, 273, 275, 277
   
-  cruiser:	.asciiz	"\nEnter the cruiser 3x[0-9a-z]: "
-  cruiser_in:	.word	
-  destroyer:	.asciiz	"\nEnter the destroyer 2x[0-9a-z]: "
-  destroy_in:	.word	
-  submarine:	.asciiz	"\nEnter the submarine [0-9a-z]: "
-  sub_in:	.word	
-  shot:		.asciiz "\nEnter the next shot [0-9a-z]: "
-  shot_input:	.space	1
-  new:		.asciiz "\nNew game? (y/n):"
-  buf:		.space	200
-  var1:		.word	3
+  cruiser:    	.asciiz    "\nEnter the cruiser 3x[0-9a-z]: "
+  cruiser_in:   .space  4    
+  destroyer:    .asciiz    "\nEnter the destroyer 2x[0-9a-z]: "
+  destroy_in:   .space  3    
+  submarine:    .asciiz    "\nEnter the submarine [0-9a-z]: "
+  sub_in:    	.space  2    
+  shot:        	.asciiz "\nEnter the next shot [0-9a-z]: "
+  shot_input:   .space    1
+  new:        	.asciiz "\nNew game? (y/n):"
+  buf:        	.space    200
+  var1:        	.word    3
 
 .text
 .globl main
 #main procedure/function in program
     main:
-	#print board
-	li	$v0,	4
-	la	$a0,	board
-	syscall
-	
-	#get ships and add to board
-	#cruiser
-	li 	$v0,	4		#loads space
-	la	$a0,	cruiser		#loads cruiser statement
-	syscall
-	
-	#get input, store in word
-	la	$a0,	cruiser_in	#sets $a0 to space allocated
-	la	$a1,	cruiser_in	#gets length of space
-	li	$v0,	8		#load opcode (8)
-	syscall				#sees 8, asks for input, puts string in $a0
+    #print board
+    li    $v0,    4
+    la    $a0,    board
+    syscall
+    
+    #get ships and add to board
+    #cruiser
+    li    $v0,    4        #loads space
+    la    $a0,    cruiser        #loads cruiser statement
+    syscall
+    
+    #get input, store in word
+    la    $a0,    cruiser_in    #sets $a0 to space allocated
+    li    $a1,    4           #gets length of space
+    li    $v0,    8        #load opcode (8)
+    syscall                #sees 8, asks for input, puts string in $a0
 
-	#gets first byte from cruiser
-	la	$t0,	cruiser_in
-	lb	$a0,	($t0)
-	li	$v0,	1
-	syscall
-	
-	#gets second byte from cruiser
-	la	$t0,	cruiser_in
-	lb	$a0,	1($t0)
-	li	$v0,	1
-	syscall
-	
-	#gets third byte from cruiser
-	la	$t0,	cruiser_in
-	lb	$a0,	2($t0)
-	li	$v0,	1
-	syscall
+    #gets first byte from cruiser
+    la    $t0,    cruiser_in
+    lb    $a0,    ($t0)
+    li    $v0,    1
+    syscall
+    
+    #gets second byte from cruiser
+    add   $t0, $t0, 1
+    lb    $a0,    ($t0)
+    li    $v0,    1
+    syscall
+    
+    #gets third byte from cruiser
+    add   $t0, $t0, 1
+    lb    $a0,    ($t0)
+    li    $v0,    1
+    syscall
 
-	#add to board
-			
-	#destroyer
-	#li 	$v0,	4
-	#la	$a0,	destroyer
-	#syscall
-	
-	#get input, store in word
-	#la	$a0,	destroy_in
-	#la	$a1,	destroy_in
-	#li	$v0,	8
-	#syscall
-	
-	#add to board
-	
-	#submarine
-	#li 	$v1,	4
-	#la	$a1,	submarine
-	#syscall
-	
-	#la	$a0,	sub_in
-	#la	$a1,	sub_in
-	#li	$v0,	8
-	#syscall
-	
-	#li	$v0,	8
-	#la	$a0,	buf
-	#li	$a1,	200
-	#syscall
-	
-	#add to board
-	
-	#print shot string
-	li 	$v0,	4	#receives string
-	la	$a0,	shot 	#address of string to print
-	syscall			#print the string
-	
-	
-	#getting shot from user
-	li	$v0,	12	#prepare to receive input
-	la	$a0,	shot_input
-	li	$a1,	1
-	syscall
-	
-       	blt   $v0, '0', not_number		#if less than 0, not a number. tests to see if letter
-       	bgt   $v0, '9', not_number		#if greater than 9, not a number. tests to see if letter
-	sub  $t0, $v0, '0'			#otherwise, subtracts difference for ascii value
-	j    add_board				#add to board
+    #add to board
+            
+    #destroyer
+    li    $v0,    4
+    la    $a0,    destroyer
+    syscall
+    
+    #get input, store in word
+    la    $a0,    destroy_in
+    li    $a1,    3
+    li    $v0,    8
+    syscall
+    #gets first byte from cruiser
+    la    $t0,    destroy_in
+    lb    $a0,    ($t0)
+    li    $v0,    1
+    syscall
+    
+    #gets second byte from cruiser
+    add   $t0, $t0, 1
+    lb    $a0,    ($t0)
+    li    $v0,    1
+    syscall
+        
+    #add to board
+    
+    #submarine
+    li    $v0,	  4
+    la    $a0,	  submarine
+    syscall
+    
+    la    $a0,    sub_in
+    li    $a1,    2
+    li    $v0,    8
+    syscall
+    
+        #gets first byte from cruiser
+    la    $t0,    sub_in
+    lb    $a0,    ($t0)
+    li    $v0,    1
+    syscall
+    
+    #add to board
+    
+    #print shot string
+    li    $v0,    4    #receives string
+    la    $a0,    shot     #address of string to print
+    syscall            #print the string
+    
+    
+    #getting shot from user
+    li    $v0,    12    #prepare to receive input
+    la    $a0,    shot_input
+    li    $a1,    1
+    syscall
+    
+    blt   $v0, '0', not_number        #if less than 0, not a number. tests to see if letter
+    bgt   $v0, '9', not_number        #if greater than 9, not a number. tests to see if letter
+    sub  $t0, $v0, '0'            #otherwise, subtracts difference for ascii value
+    j    add_board                #add to board
 
-not_number:    	blt   $v0, 'a', not_letter	#if v0 is less than a, not a letter. 
-        	bgt   $v0, 'z', not_letter	#if v0 is more than z, not a letter.
-        	sub   $t0, $v0, 'a'		#otherwise, subtract/add the difference of ascii value
-        	add   $t0, $t0, 10		
-		
-add_board:	mul  $t0, $t0, 2         # Each offset is two-byte long.
-    		lh   $t1, offset1($t0)   # Load $t1 with the offset of the index $t0.
-     		li   $t2, 'X'            # Put the marker X in $t2.
-     		sb   $t2, board($t1)
-     		 
-not_letter:    	la      $a0, board	#prints board again
-        	li      $v0, 4
-        	syscall
-	
-	# Exit the program.
-       li      $v0, 	10
+not_number: blt   $v0, 'a', not_letter    #if v0 is less than a, not a letter. 
+            bgt   $v0, 'z', not_letter    #if v0 is more than z, not a letter.
+            sub   $t0, $v0, 'a'        #otherwise, subtract/add the difference of ascii value
+            add   $t0, $t0, 10        
+        
+add_board:  mul  $t0, $t0, 2         # Each offset is two-byte long.
+            lh   $t1, offset1($t0)   # Load $t1 with the offset of the index $t0.
+            li   $t2, 'X'            # Put the marker X in $t2.
+            sb   $t2, board($t1)
+              
+not_letter: la      $a0, board    #prints board again
+            li      $v0, 4
+            syscall
+    
+    # Exit the program.
+       li      $v0,     10
        syscall
-	
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+    
