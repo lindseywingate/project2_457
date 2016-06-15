c7f1402126a409d7717eff46b1644853f0d4318a
Lindsey Wingate <lindseymwingate@gmail.com>
2016-06-14 19:32:20 -0500

    made some changes, no success. trying to understand subroutines

--- 370/program_1.asm
+++ 370/program_1.asm
@@ -20,6 +20,7 @@
              .half   218, 220, 222, 224, 226, 228
              .half   267, 269, 271, 273, 275, 277
   
+  invalid:	.asciiz    "\nYour input is invalid.\n"
   cruiser:    	.asciiz    "\nEnter the cruiser 3x[0-9a-z]: "
   cruiser_in:   .space  4    
   destroyer:    .asciiz    "\nEnter the destroyer 2x[0-9a-z]: "
@@ -57,18 +58,21 @@
     la    $t0,    cruiser_in
     lb    $a0,    ($t0)
     li    $v0,    1
+    jal	  find_value	#add to board
     syscall
     
     #gets second byte from cruiser
     add   $t0, $t0, 1
     lb    $a0,    ($t0)
     li    $v0,    1
+    jal	  find_value	#add to board
     syscall
     
     #gets third byte from cruiser
     add   $t0, $t0, 1
     lb    $a0,    ($t0)
     li    $v0,    1
+    jal	  find_value	#add to board
     syscall
 
     #add to board
@@ -107,7 +111,7 @@
     li    $v0,    8
     syscall
     
-        #gets first byte from cruiser
+        #gets first byte from sub to add mark
     la    $t0,    sub_in
     lb    $a0,    ($t0)
     li    $v0,    1
@@ -117,20 +121,29 @@
     
     #print shot string
     li    $v0,    4    #receives string
-    la    $a0,    shot     #address of string to print
+    la    $a0,    shot #address of string to print
     syscall            #print the string
-    
     
     #getting shot from user
     li    $v0,    12    #prepare to receive input
     la    $a0,    shot_input
-    li    $a1,    1
+    li    $a1,    1   
     syscall
+  
+    la   $a0, board    #prints board again
+    li   $v0, 4
+    syscall
+  
+    li	$v0,	10
+    syscall
+    ## subroutines ##
     
-    blt   $v0, '0', not_number        #if less than 0, not a number. tests to see if letter
-    bgt   $v0, '9', not_number        #if greater than 9, not a number. tests to see if letter
-    sub  $t0, $v0, '0'            #otherwise, subtracts difference for ascii value
-    j    add_board                #add to board
+find_value:  
+            li    $v0, 4
+	    blt   $v0, '0', not_number    #if less than 0, not a number. tests to see if letter
+    	    bgt   $v0, '9', not_number    #if greater than 9, not a number. tests to see if letter
+    	    sub  $t0, $v0, '0'            #otherwise, subtracts difference for ascii value
+    	    j    add_board                #add to board
 
 not_number: blt   $v0, 'a', not_letter    #if v0 is less than a, not a letter. 
             bgt   $v0, 'z', not_letter    #if v0 is more than z, not a letter.
@@ -141,9 +154,14 @@ add_board:  mul  $t0, $t0, 2         # Each offset is two-byte long.
             lh   $t1, offset1($t0)   # Load $t1 with the offset of the index $t0.
             li   $t2, 'X'            # Put the marker X in $t2.
             sb   $t2, board($t1)
-              
-not_letter: la      $a0, board    #prints board again
-            li      $v0, 4
+            jr   $ra
+
+  	    la   $a0, board    #prints board again
+            li   $v0, 4
+                        
+not_letter: la   $a0, board    #prints board again
+            li   $v0, 4
+            jr	 $ra
             syscall
     
     # Exit the program.
