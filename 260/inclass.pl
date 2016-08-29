0ccba5348c472946e89d35787c8166e4c30fc298
Lindsey Wingate <lindseymwingate@gmail.com>
2016-08-29 14:28:31 -0500

    added inclass notes

---
+++ 260/inclass.pl
@@ -0,0 +1,19 @@
+use strict;
+my $value1;
+
+$vehicle = "motorcycle";
+print "he drives a", $vehicle, "\n";
+
+$value1 = 123.456;
+$value2 = "45";
+$value3 = 2;
+
+$answer1 = $value1 * $value2;
+$answer2 = $value2 + $value3;
+$answer3 = substr($value1, 3, 1);
+
+print "Answer1 is $answer1\n";
+print "Answer2 is $answer2\n";
+print "Answer3 is $answer3\n";
+$answer1 = "34woeifjdslkd" * "2aldkjfaldf";
+print "Answer1 is $answer1\n";
--- 370/first_program.asm
+++
@@ -1,9 +0,0 @@
-.data #always has data and text section
-    mymessage:    .asciiz    "Hello, World!\n"
-
-.text #instructions
-    li $v0, 4 #prepare for printing-putting value 4 into register shows you want to print to screen
-    la $a0, mymessage #put value (hello world) into register
-    syscall #do now
-    
-    
