f3433d96911d90c7d4de5fdf194ea1a268304f54
Lindsey Wingate <lindseymwingate@gmail.com>
2016-09-07 13:42:25 -0500

    got hw2-A working the way I like.

--- 451/hw2-A-lindseywingate.c
+++ 451/hw2-A-lindseywingate.c
@@ -26,20 +26,19 @@ int input;
 		if (PID1 == 0) {
 			printf("CHILD: Hello from child.\n");
 			printf("CHILD: Starting XCLOCK..\n");
-
 			/* Start XCLOCK in the forked process.                 */
 			execlp(path1, arg1_0, arg1_1);
 		}
-	/* The parent's code.                                     */
-	   if (PID1 != 0) {
-			printf("Enter 1 to kill off processes.");
-			scanf("%d", &input);
-			if(input==1) {
-				printf("PARENT: Child exited with code: %d\n", kill(PID1, 9));
-				waitpid(PID1, &ExitCode1, 0);
-			}	
-		}
 		counter++;
+	}
+/* The parent's code.                                     */
+   if (PID1 != 0) {
+		printf("\nENTER 1 TO KILL OFF PROCESSES.\n\n");
+		scanf("%d", &input);
+		if(input==1) {
+			printf("PARENT: Child exited with code: %d\n", kill(PID1, 9));
+			waitpid(PID1, &ExitCode1, 0);
+		}	
 	}
 return 0;   
 }
