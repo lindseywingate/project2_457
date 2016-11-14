b985b250e698b3b81726eb763542f7e688f09b77
Lindsey Wingate <lindseymwingate@gmail.com>
2016-11-14 14:39:17 -0600

    adding stuff for perl.

---
+++ 260/firstPerlScript.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+use CGI qw:standard;
+
+print "Content-type: text/html\n\n";
+
+print "<html>";
+print "<header><title>First Perl Script</title></header>";
+print "<body>";
+print "First Perl Script\n\n";
+print "I hope it works";
+print "</body>";
+print "</html>";
