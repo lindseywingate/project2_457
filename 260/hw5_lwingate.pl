a7dd4735d6e917660996f2fd1a7964476bd0eee5
Lindsey Wingate <lindseymwingate@gmail.com>
2016-11-29 19:23:28 -0600

    started working on hw5 for perl :D

---
+++ 260/hw5_lwingate.pl
@@ -0,0 +1,8 @@
+/*Lindsey Wingate, 1011265, Program 5
+This program allows a user to interact with a database of songs. The user may add a song, update a song, delete a song, or search for a song by artist\/title or name of album.
+*/
+
+
+
+
+/*This code was written by Lindsey Wingate*/
