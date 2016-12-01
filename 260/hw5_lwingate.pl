aca5da5d0d3291426a3368e013dfb1ddbce953cf
Lindsey Wingate <lindseymwingate@gmail.com>
2016-11-30 21:07:44 -0600

    hw5 working, just need queries.

--- 260/hw5_lwingate.pl
+++ 260/hw5_lwingate.pl
@@ -17,28 +17,58 @@ if(!$dbh) {
 }
 
 while(1) {
-	print "\n\tPlease make a selection. Press 0 to exit.\n
-	Press a to add a new song.\n
-	Press b to update an existing song.\n
-	Press c to print a list of existing songs.\n
-	Press d to search by artist, title, or both.";
+	print "\nPlease make a selection. Press 0 to exit.\nPress a to add a new song.\nPress b to update an existing song.\nPress c to print a list of existing songs.\nPress d to search by artist or title.\nPress e to search by album title.\n";
+
 	my $main_sel = <STDIN>;
 	chomp($main_sel);
 	if($main_sel eq '0') {
 		exit;
 	}
 	if($main_sel eq 'a') {
-		print"test1";
+		print "Press 1 to go back to main.\nEnter Artist: \n";
+		my $artist = <STDIN>;
+		chomp($artist);
+		if($artist eq '1') {
+			next;
+		}
+		print "Enter Song Title: \n";
+		my $song = <STDIN>;
+		chomp($song);
+		print "Enter Album Title: \n";
+		my $album = <STDIN>;
+		chomp($album);
+		print "Enter Length of Song. Must be in format Minutes:Seconds: \n";
+		my $song_length = <STDIN>;
+		chomp($song_length);
 	}
 	if($main_sel eq 'b') {
-		print"test2";
+		print "Press 1 to go back to main.\nEnter existing song.\n";
+		my $song = <STDIN>;
+		chomp($song);
+		if($song eq '1') {
+			next;
+		}
+		
 	}
 	if($main_sel eq 'c') {
-		print"test3";
+		#print list of existing songs from database
+		next;	
+	
 	}
 	if($main_sel eq 'd') {
-		print"test4";
+		print "Press 1 to go back to main.\nEnter an artist or song title.\n";
+		my $artist_song = <STDIN>;
+		chomp($artist_song);
+		next;
 	}
+	if($main_sel eq 'e') {
+		print "Press 1 to go back to main.\nEnter an album title.\n";
+		my $album = <STDIN>;
+		chomp($album);
+		next;
+	}
+
+
 }
 
 #This code was written by Lindsey Wingate
