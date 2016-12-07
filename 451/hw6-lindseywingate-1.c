766211a61f26ac8545642595afc473d558691841
Lindsey Wingate <lindseymwingate@gmail.com>
2016-12-06 20:27:11 -0600

    got arguments converting from strings to ints in babypig

--- 451/hw6-lindseywingate-1.c
+++ 451/hw6-lindseywingate-1.c
@@ -6,6 +6,27 @@
 #include <stdio.h>
 #include <semaphore.h>
 
+int toString(char a[]) {
+  int c, sign, offset, n;
+  if (a[0] == '-') {  // Handle negative integers
+    sign = -1;
+  }
+  if (sign == -1) {  // Set starting position to convert
+    offset = 1;
+  }
+  else {
+    offset = 0;
+  }
+  n = 0;
+  for (c = offset; a[c] != '\0'; c++) {
+    n = n * 10 + a[c] - '0';
+  }
+  if (sign == -1) {
+    n = -n;
+  }
+  return n;
+}
+
 int main(void) {
 	int value;
 	int semid;
