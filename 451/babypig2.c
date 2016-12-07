213a77c6f48200a92cbe8e22c1d03f93ea59617d
Lindsey Wingate <lindseymwingate@gmail.com>
2016-12-06 19:37:15 -0600

    GOT PIPE WORKING!!!!!!!!!!!!!!!!!!!!!!!!!!

--- 451/babypig2.c
+++ 451/babypig2.c
@@ -7,9 +7,12 @@
 #include <sys/sem.h>
 
 int main(int argc, char *argv[]) {
-	printf("\nFILENAME: %s\n PIPEID %s\n SEMID %s\n SHAREDMEMID %s\n", argv[0], argv[1], argv[2], argv[3]);
+	//printf("\nFILENAME: %s\n PIPEID %s\n SEMID %s\n SHAREDMEMID %s\n", argv[0], argv[1], argv[2], argv[3]);
 
-		
-
-
+	FILE*stuff;
+	int c;
+	stuff = fopen(argv[0], "r");
+	while((c=fgetc(stuff)) !=EOF)
+		putchar(c);
+	fclose(stuff);		
 }
