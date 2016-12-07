ac4bbddf1bc0cba7397b17e52b2bf9275612f4dc
Lindsey Wingate <lindseymwingate@gmail.com>
2016-12-06 19:56:11 -0600

    got random num generator working

--- 451/babypig2.c
+++ 451/babypig2.c
@@ -5,6 +5,8 @@
 #include <semaphore.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
 	//printf("\nFILENAME: %s\n PIPEID %s\n SEMID %s\n SHAREDMEMID %s\n", argv[0], argv[1], argv[2], argv[3]);
@@ -12,7 +14,14 @@ int main(int argc, char *argv[]) {
 	FILE*stuff;
 	int c;
 	stuff = fopen(argv[0], "r");
+
+	time_t t;
+	srand((unsigned) time(&t));
+	int r = rand() % 10;
+
 	while((c=fgetc(stuff)) !=EOF)
 		putchar(c);
-	fclose(stuff);		
+	fclose(stuff);	
+	printf("NUMBER IS: %d", r);
+	return 0;	
 }
