ef84617d8d0d84dd0d2efa93d4eacef843d1375f
Lindsey Wingate <lindseymwingate@gmail.com>
2017-04-11 20:27:38 +0200

    now i just need to work on the individual element pages and hooking them up to the server. Good work :)

--- AplicacaodeLivros/app/src/main/java/com/example/aplicacaodelivros/SigninActivity.java
+++ AplicacaodeLivros/app/src/main/java/com/example/aplicacaodelivros/SigninActivity.java
@@ -1,8 +1,6 @@
 package com.example.aplicacaodelivros;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.widget.TextView;
@@ -11,10 +9,6 @@ import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
-import static com.example.aplicacaodelivros.R.id.textView3;
-import static com.example.aplicacaodelivros.R.id.textView4;
 
 public class SigninActivity extends AsyncTask<String, Void, String> {
         private TextView statusField, roleField;
@@ -64,7 +58,7 @@ public class SigninActivity extends AsyncTask<String, Void, String> {
     protected void onPostExecute(String result) {
         if (result!=null) {
             super.onPostExecute(result);
-            context.startActivity(new Intent(context, Homepage.class));
+            context.startActivity(new Intent(context, HomepageActivity.class));
         }
     }
 }
