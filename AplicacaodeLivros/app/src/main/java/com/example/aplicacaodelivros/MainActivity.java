55c47a1e20b725c4b33df196e4272b217ecb5b01
Lindsey Wingate <lindseymwingate@gmail.com>
2017-04-28 15:51:44 +0200

    actually totally for sure got login working for one, static user. will build app around this.

--- AplicacaodeLivros/app/src/main/java/com/example/aplicacaodelivros/MainActivity.java
+++ AplicacaodeLivros/app/src/main/java/com/example/aplicacaodelivros/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
+import android.preference.PreferenceManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,33 +12,33 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import static com.example.aplicacaodelivros.R.id.editText;
+import static com.example.aplicacaodelivros.R.id.icon;
 
 public class MainActivity extends Activity {
-    public static final String MyPREFERENCES = "MyPrefs";
-    public static final String Name = "nameKey";
-    public static final String Password = "passwordKey";
-
     EditText usernameField, passwordField;
     Button button;
 
     SharedPreferences sharedpreferences;
+
     @Override
     //identify fields where you are pulling in info from user
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        usernameField = (EditText)findViewById(R.id.editText);
-        passwordField = (EditText)findViewById(R.id.editText2);
-
     }
 
     public void loginGet (View view) {
-        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedpreferences.edit();
+        usernameField = (EditText)findViewById(R.id.editText);
+        passwordField = (EditText)findViewById(R.id.editText2);
+        String username = usernameField.getEditableText().toString();
+        String password = passwordField.getEditableText().toString();
 
-        editor.putString(Name, usernameField.toString());
-        editor.putString(Password, passwordField.toString());
-        editor.commit();
-        new SigninActivity(this).execute(usernameField.toString(), passwordField.toString());
+        if(username.equals("customer") && password.equals("testing")) {
+            setContentView(R.layout.activity_homepage);
+        }
+        else {
+            TextView incorrectlogin = (TextView) findViewById(R.id.incorrectlogin);
+            incorrectlogin.setText("Incorrect Login");
+        }
     }
 }
