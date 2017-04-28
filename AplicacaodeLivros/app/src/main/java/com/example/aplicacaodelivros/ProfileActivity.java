14aed26ae8cc47c24d599f93f14ac48fc80645be
Lindsey Wingate <lindseymwingate@gmail.com>
2017-04-28 21:28:15 +0200

    not working still!!!! UGHHHHHHHH

--- AplicacaodeLivros/app/src/main/java/com/example/aplicacaodelivros/ProfileActivity.java
+++ AplicacaodeLivros/app/src/main/java/com/example/aplicacaodelivros/ProfileActivity.java
@@ -1,34 +1,66 @@
 package com.example.aplicacaodelivros;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
-public class ProfileActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.io.OutputStreamWriter;
 
-    private String usernameField, passwordField;
+public class ProfileActivity extends AsyncTask<String, Void, String> {
+        private Context context;
 
-    //Bundle b=this.getIntent().getExtras();
-    //String[] array = b.getStringArray(null);
+        public ProfileActivity(Context context) {
+            this.context = context;
+        }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        protected void onpreExecute() {} //parent class
 
-        SharedPreferences sharedpreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String username = sharedpreferences.getString("Name", "");
-        String password = sharedpreferences.getString("Password", "");
+        @Override //do not want to call parent method. use super if you want both
+        protected String doInBackground(String... arg0) {
+            try {
+                //search for phrase
+                //php script to call
+                String link = "http://people.aero.und.edu/~lwingate/457/2/profile_get.php";
 
-        TextView textView = (TextView) findViewById(R.id.textView5);
-        TextView textView1 = (TextView) findViewById(R.id.textView8);
-        textView.setText(username);
-        textView.setText(password);
+                //get method to pass variables
+                link += "?username=" + URLEncoder.encode("customer", "UTF-8");
+                link += "&password=" + URLEncoder.encode("testing", "UTF-8");
 
+                //Connect to server
+                URL url = new URL(link);
+                URLConnection conn = url.openConnection();
+                conn.setDoOutput(true);
+
+                //Read server response
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(conn.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                    break;
+                }
+                return sb.toString();
+            }
+            catch(Exception e) {
+                return new String("Exception: " + e.getMessage());
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) { //result of background computation passed here after doneif (result!=null) { //if returns with data, entry exists. login successful
+            super.onPostExecute(result);
+            Intent newintent = new Intent();
+            newintent.setClass(context, SearchResultsActivity.class);
+            context.startActivity(newintent);
+            //context.startActivity(new Intent(context, SearchResultsActivity.class));
+        }
     }
-}
